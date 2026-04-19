c172ee29e246f2660aafeadf413412f7710dd618
CWmain <cameronwhite2478@gmail.com>
2026-04-19 22:37:31 +1000

    Up to state machine, need to fix vibrating camera

---
+++ Objects/Player/camera_controller.gd
@@ -0,0 +1,28 @@
+class_name CameraController extends Node3D
+
+@export var debug: bool = false
+@export_category("References")
+@export var player_controller: PlayerController
+@export var component_mouse_capture: mouseCapture
+@export_category("Camera Settings")
+@export_group("Camera Tilt")
+@export_range(-PI/2, PI/3) var tilt_lower_limit: float = -PI/2
+@export_range(PI/3, PI/2) var tilt_upper_range: float = PI/2
+
+var _rotation: Vector3
+
+func _process(delta: float) -> void:
+	update_camera_rotation(component_mouse_capture._mouse_input)
+
+func update_camera_rotation(input: Vector2) -> void:
+	_rotation.x += input.y
+	_rotation.y += input.x
+	_rotation.x = clamp(_rotation.x, tilt_lower_limit, tilt_upper_range)
+	
+	var _player_rotation = Vector3(0.0, _rotation.y, 0.0)
+	var _camera_rotation = Vector3(_rotation.x, 0.0 ,0.0)
+	
+	transform.basis = Basis.from_euler(_camera_rotation)
+	player_controller.update_rotation(_player_rotation)
+	
+	_rotation.z = 0
--- Objects/Player/player.gd
+++ Objects/Player/player.gd
@@ -1,6 +1,30 @@
-extends CharacterBody3D
+class_name PlayerController extends CharacterBody3D
 
-func _process(delta: float) -> void:
+@export var acceleration: float = 2
+@export var deceleration: float = 2
+
+var _input_dir : Vector2 = Vector2.ZERO
+var _movement_velocity : Vector3 = Vector3.ZERO
+var speed : float = 5.0
+
+func update_rotation(rotation_input) -> void:
+	global_transform.basis = Basis.from_euler(rotation_input)
+
+func _physics_process(delta: float) -> void:
 	if !is_on_floor():
-		velocity.y = -1
+		velocity += get_gravity() * delta
+		
+	_input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+
+	var current_velocity = Vector2(_movement_velocity.x, _movement_velocity.z)
+	var direction = (transform.basis * Vector3(_input_dir.x, 0.0, _input_dir.y)).normalized()
+	print(direction)
+	
+	if direction:
+		current_velocity = lerp(current_velocity, Vector2(direction.x, direction.z) * speed, acceleration)
+	else:
+		current_velocity = current_velocity.move_toward(Vector2.ZERO, deceleration)
+	
+	_movement_velocity = Vector3(current_velocity.x, velocity.y, current_velocity.y)
+	velocity = _movement_velocity
 	move_and_slide()
